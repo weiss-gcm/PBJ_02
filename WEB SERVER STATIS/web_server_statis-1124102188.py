abff1b4f9eba1afa942641cb641e37a9352760d5
Weiss_GCM <darma20060311@gmail.com>
2025-06-12 12:11:46 +0700

    Update web_server_statis-1124102188.py

--- WEB SERVER STATIS/web_server_statis-1124102188.py
+++ WEB SERVER STATIS/web_server_statis-1124102188.py
@@ -17,7 +17,10 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes('<img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/lasagna.jpg" alt="lasagna">', "utf-8"))
+        self.wfile.write(bytes('<img src="https://cdn.freecodecamp.org/curriculum/cat-photo-app/lasagna.jpg" alt="makanan">', "utf-8"))
+        self.wfile.write(bytes('<img src="https://en.wikichip.org/w/images/4/4e/amd_ryzen_7_logo.png" alt="cpu">', "utf-8"))
+        self.wfile.write(bytes('<img src="https://images.squarespace-cdn.com/content/v1/607f89e638219e13eee71b1e/1684821560422-SD5V37BAG28BURTLIXUQ/michael-sum-LEpfefQf4rU-unsplash.jpg" alt="Kucing">', "utf-8"))
+        self.wfile.write(bytes('<img src="https://www.ginoguitars.com/libimg2/products/2160x1440/0/90/Fender-American-Original-60s-Telecaster-099-RW-LPB_01.jpg" alt="Telecaster 60s">', "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":        
